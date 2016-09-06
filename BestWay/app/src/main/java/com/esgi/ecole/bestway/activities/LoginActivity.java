d2ee432b17d3bc58264362551fd8825873085fc7
Mohsen3861 <mohsenraic@hotmail.fr>
2016-09-06 16:42:55 +0200

    structure

---
+++ BestWay/app/src/main/java/com/esgi/ecole/bestway/activities/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.esgi.ecole.bestway.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.esgi.ecole.bestway.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
