95f9bb4f07cb028829a043c66dc6dc46ed416450
Mohsen3861 <mohsenraic@hotmail.fr>
2016-09-07 09:54:53 +0200

    IHM home page

--- BestWay/app/src/main/java/com/esgi/ecole/bestway/activities/LoginActivity.java
+++ BestWay/app/src/main/java/com/esgi/ecole/bestway/activities/LoginActivity.java
@@ -1,15 +1,35 @@
 package com.esgi.ecole.bestway.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.esgi.ecole.bestway.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    public Button connexionButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        assignViews();
+
+        connexionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(LoginActivity.this , MainActivity.class);
+                startActivity(i);
+
+            }
+        });
+    }
+
+    public void assignViews(){
+        connexionButton = (Button) findViewById(R.id.buttonConnexion);
+
     }
 }
