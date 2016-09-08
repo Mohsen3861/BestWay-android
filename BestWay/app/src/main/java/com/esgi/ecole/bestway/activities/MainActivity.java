3c5a8150280a1bc45618bb8ee68568c942d3f8f8
Mohsen3861 <mohsenraic@hotmail.fr>
2016-09-08 18:33:46 +0200

    geoloc done

--- BestWay/app/src/main/java/com/esgi/ecole/bestway/activities/MainActivity.java
+++ BestWay/app/src/main/java/com/esgi/ecole/bestway/activities/MainActivity.java
@@ -1,6 +1,11 @@
 package com.esgi.ecole.bestway.activities;
 
+import android.Manifest;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
@@ -9,39 +14,60 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.esgi.ecole.bestway.R;
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.PlaceDetectionApi;
+import com.google.android.gms.location.places.PlaceLikelihood;
+import com.google.android.gms.location.places.PlaceLikelihoodBuffer;
+import com.google.android.gms.location.places.Places;
 import com.google.android.gms.location.places.ui.PlaceAutocomplete;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
 
     Button goButton;
     EditText departEditText;
     EditText arriveEditText;
+    ImageView locationImage;
 
     public static String TAG = "GOOGLE PLACES";
 
     private static final int REQUEST_CODE_AUTOCOMPLETE_DEPART = 1;
     private static final int REQUEST_CODE_AUTOCOMPLETE_ARRIVE = 2;
 
+    int GOOGLE_API_CLIENT_ID = 0;
+
+
+    private GoogleApiClient mGoogleApiClient;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setTitle(getString(R.string.home_page_title));
 
+
+         mGoogleApiClient = new GoogleApiClient.Builder(MainActivity.this)
+                .addApi(Places.GEO_DATA_API)
+                .enableAutoManage(this, GOOGLE_API_CLIENT_ID, this)
+                .addApi(Places.PLACE_DETECTION_API)
+                .build();
+
         assignViews();
 
         goButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this , ResulatActivity.class);
+                Intent i = new Intent(MainActivity.this, ResulatActivity.class);
                 startActivity(i);
             }
         });
@@ -59,16 +85,23 @@ public class MainActivity extends AppCompatActivity {
                 openAutocompleteActivity(REQUEST_CODE_AUTOCOMPLETE_ARRIVE);
             }
         });
+
+        locationImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getCurrentPlace();
+            }
+        });
     }
 
-    public void assignViews(){
+    public void assignViews() {
 
         goButton = (Button) findViewById(R.id.buttonGo);
         departEditText = (EditText) findViewById(R.id.editTextDepart);
         arriveEditText = (EditText) findViewById(R.id.editTextArrive);
+        locationImage = (ImageView) findViewById(R.id.location_icon);
 
     }
-
 
 
     /**
@@ -80,36 +113,37 @@ public class MainActivity extends AppCompatActivity {
 
         // Check that the result was from the autocomplete widget.
         if (requestCode == REQUEST_CODE_AUTOCOMPLETE_DEPART) {
-                // Get the user's selected place from the Intent.
-                Place place = PlaceAutocomplete.getPlace(this, data);
-                Log.i(TAG, "Place Selected: " + place.getName());
+
+            // Get the user's selected place from the Intent.
+            Place place = PlaceAutocomplete.getPlace(this, data);
+            Log.i(TAG, "Place Selected: " + place.getName());
 
 
-                // Format the place's details and display them in the TextView.
-                departEditText.setText(place.getAddress());
+            // Format the place's details and display them in the TextView.
+            departEditText.setText(place.getAddress());
 
         }
 
         if (requestCode == REQUEST_CODE_AUTOCOMPLETE_ARRIVE) {
             //if (resultCode == RESULT_OK) {
-                // Get the user's selected place from the Intent.
-                Place place = PlaceAutocomplete.getPlace(this, data);
-                Log.i(TAG, "Place Selected: " + place.getName());
+            // Get the user's selected place from the Intent.
+            Place place = PlaceAutocomplete.getPlace(this, data);
+            Log.i(TAG, "Place Selected: " + place.getName());
 
 
-                // Format the place's details and display them in the TextView.
-                arriveEditText.setText(place.getAddress());
+            // Format the place's details and display them in the TextView.
+            arriveEditText.setText(place.getAddress());
 
 
         }
     }
 
 
-
     private void openAutocompleteActivity(int requestCode) {
         try {
             // The autocomplete activity requires Google Play Services to be available. The intent
             // builder checks this and throws an exception if it is not the case.
+
             Intent intent = new PlaceAutocomplete.IntentBuilder(PlaceAutocomplete.MODE_FULLSCREEN)
                     .build(this);
             startActivityForResult(intent, requestCode);
@@ -127,5 +161,49 @@ public class MainActivity extends AppCompatActivity {
             Log.e("GOOGLE PLACES", message);
             Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
         }
+    }
+
+
+    public void getCurrentPlace() {
+
+        final ProgressDialog dialog = ProgressDialog.show(MainActivity.this, "",
+                "Chargement ... ", true);
+
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            dialog.dismiss();
+            return;
+        }
+        PendingResult<PlaceLikelihoodBuffer> result = Places.PlaceDetectionApi
+                .getCurrentPlace(mGoogleApiClient, null);
+        result.setResultCallback(new ResultCallback<PlaceLikelihoodBuffer>() {
+            @Override
+            public void onResult(PlaceLikelihoodBuffer likelyPlaces) {
+                for (PlaceLikelihood placeLikelihood : likelyPlaces) {
+                    Log.i(TAG, String.format("Place '%s' has likelihood: %g",
+                            placeLikelihood.getPlace().getName(),
+                            placeLikelihood.getLikelihood()));
+                    departEditText.setText(placeLikelihood.getPlace().getAddress());
+                }
+
+                likelyPlaces.release();
+                dialog.dismiss();
+            }
+        });
+
+
+
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mGoogleApiClient.stopAutoManage(this);
+        mGoogleApiClient.disconnect();
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
